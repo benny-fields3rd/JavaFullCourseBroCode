b5c6ff5fdb0cbc1a2fad05650c574c2d5849e96b
Benny Fields <befields@redhat.com>
2022-02-02 23:47:07 -0600

    Added lesson files

---
+++ src/GenericsClasses.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class GenericsClasses {
+
+    public static void main(String[] args) {
+
+        //bounded types = 	you can create the objects of a generic class to have data
+        //					of specific derived types ex.Number
+
+        MyGenericClass<Integer, Integer> myInt = new MyGenericClass<>(1, 9);
+        MyGenericClass<Double, Double> myDouble = new MyGenericClass<>(3.14, 1.01);
+
+
+        //MyGenericClass<Character, Character> myChar = new MyGenericClass<>('@', '$');
+        //MyGenericClass<String, Character> myString = new MyGenericClass<>("Hello", '!');
+
+        ArrayList<String> myFriends = new ArrayList<>();
+
+        System.out.println(myInt.getValue());
+        System.out.println(myDouble.getValue());
+        //System.out.println(myChar.getValue());
+        //System.out.println(myString.getValue());
+
+    }
+
+}
